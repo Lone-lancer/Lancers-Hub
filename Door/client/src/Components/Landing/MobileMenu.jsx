f0cca898cc90d337aafffd77884737b3bc479778
Abenezer Sisay <abenezerxsisay@gmail.com>
2025-01-24 02:32:00 +0300

    mobile menu updated

--- Door/client/src/Components/Landing/MobileMenu.jsx
+++ Door/client/src/Components/Landing/MobileMenu.jsx
@@ -7,131 +7,18 @@ import { Link } from "react-router-dom";
 const menus = [
   {
     id: 1,
-    title: "Home",
-    link: "/home",
-    submenu: [
-      {
-        id: 11,
-        title: "Home 1",
-        link: "/home",
-      },
-      {
-        id: 12,
-        title: "Home 2",
-        link: "/home-2",
-      },
-      {
-        id: 13,
-        title: "Home 3",
-        link: "/home-3",
-      },
-      {
-        id: 14,
-        title: "Home 4",
-        link: "/home-4",
-      },
-    ],
+    title: "Login",
+    link: "/login",
   },
   {
     id: 2,
-    title: "About",
-    link: "/about",
+    title: "Register",
+    link: "/register",
   },
   {
     id: 3,
-    title: "Donations",
-    link: "/donation-listing",
-    submenu: [
-      {
-        id: 31,
-        title: "Donations Listing",
-        link: "/donation-listing",
-      },
-      {
-        id: 32,
-        title: "Donations Details",
-        link: "/donation-details/Gift-an-Education-Make-a-Life-Better!",
-      },
-    ],
-  },
-  {
-    id: 4,
-    title: "Blog",
-    link: "/blog",
-    submenu: [
-      {
-        id: 41,
-        title: "Blog",
-        link: "/blog",
-      },
-      {
-        id: 42,
-        title: "Blog Details",
-        link: "/blog-details/Start-a-fundraiser-for-yourself-in-World-Charity-Day",
-      },
-    ],
-  },
-  {
-    id: 5,
-    title: "Pages",
-    link: "/events",
-    submenu: [
-      {
-        id: 51,
-        title: "Events",
-        link: "/events",
-      },
-      {
-        id: 52,
-        title: "Event Single",
-        link: "/event-single/The-Human-rights-and-democracy-programme",
-      },
-      {
-        id: 51,
-        title: "Service",
-        link: "/service",
-      },
-      {
-        id: 52,
-        title: "Service Single",
-        link: "/service-single/Study-&-Life-Tips",
-      },
-      {
-        id: 53,
-        title: "Shop",
-        link: "/products",
-      },
-      {
-        id: 54,
-        title: "Shop Single",
-        link: "/product-single/Yellow-Rocks-Umbrealla",
-      },
-      {
-        id: 55,
-        title: "Carts",
-        link: "/cart",
-      },
-      {
-        id: 56,
-        title: "Checkout",
-        link: "/checkout",
-      },
-      {
-        id: 345,
-        title: "Volunteers",
-        link: "/volunteers",
-      },
-      {
-        id: 3454,
-        title: "Volunteer Single",
-        link: "/team-single/Cameron-Williamson",
-      },
-    ],
-  },
-  {
-    id: 88,
-    title: "Contact",
-    link: "/contact",
+    title: "Get Started",
+    link: "/offices",
   },
 ];
 
